97633c80688064a923704a250b2965e071fe8c21
Elisa Praduroux <epraduro@c2r1p4.42nice.fr>
2023-01-18 15:11:59 +0100

    defini buffer size dans .h

--- get_next_line.h
+++ get_next_line.h
@@ -6,12 +6,16 @@
 /*   By: elisa <elisa@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/24 12:27:42 by elisa             #+#    #+#             */
-/*   Updated: 2023/01/02 15:16:04 by elisa            ###   ########.fr       */
+/*   Updated: 2023/01/18 15:03:35 by epraduro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1
+# endif
 
 # include <stdlib.h>
 # include <sys/types.h>
